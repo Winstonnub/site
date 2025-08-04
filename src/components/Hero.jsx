9db1ac7dd6140ee6c42e386d930f7c40357987f1
Winstonnub <cywinston.liang@mail.utoronto.ca>
2025-08-04 22:41:09 +0800

    Add gh-pahe functions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         {/* Right side: Image */}
         <div className="flex-shrink-0 ml-8">
           <img
-            src="src/assets/myself.jpeg"
+            src="site/src/assets/myself.jpeg"
             alt="Winston"
             className="w-64 h-64 object-cover rounded-full shadow-lg"
           />
@@ -31,7 +31,7 @@ const Hero = () => {
         <ul className="text-lg text-gray-200 list-disc list-inside">
           <li className="text-1xl flex flex-row font-bold">          
             <img
-            src="src/assets/uoft.jpeg"
+            src="site/src/assets/uoft.jpeg"
             alt="uoft"
             className="w-16 h-16 object-cover rounded shadow-lg mr-4"
           />
